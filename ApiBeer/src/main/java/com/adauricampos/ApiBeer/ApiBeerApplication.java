f90432e60530340ca5922d8052e7349ac796036a
adauricampos <adauridicampos@gmail.com>
2021-08-02 16:14:43 -0300

    adicionando arquivos

---
+++ ApiBeer/src/main/java/com/adauricampos/ApiBeer/ApiBeerApplication.java
@@ -0,0 +1,13 @@
+package com.adauricampos.ApiBeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBeerApplication.class, args);
+	}
+
+}
